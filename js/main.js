fac60f362d8968fc5734e50404c3004198d03837
Karolína Stachura <karolina.holinska@qest.cz>
2024-11-25 20:33:27 +0100

    fixed layout shifts - bar visibility

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
-function showTopBar(){
-    let country = "France";
-    let vat = 20;
-    setTimeout(() => {
-        document.querySelector("section.country-bar")
-        .innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`
-        document.querySelector("section.country-bar")
-        .classList.remove('hidden')
-    }, 1000);
+// Simulating response from server
+function showTopBar() {
+  let country = "Czech Republic";
+  let vat = 21;
+  setTimeout(() => {
+    document.querySelector(
+      "section.country-bar"
+    ).innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`;
+    document.querySelector("section.country-bar");
+  }, 1000);
 }
 
-
-
 showTopBar();
-
-
-
-
